80bfd3e531acca0b53fb6d5f0caf953b731fbddc
Travis Payne <travis.payne@uea.ac.uk>
2019-06-10 23:30:43 +0100

    Exporting to CSV via email
    Exports to CSV but is multithreaded as it takes a while, user can go do something else and the resulting file should be emailed to them!

--- KrillApp/static/KrillApp/Javascript/view_trips.js
+++ KrillApp/static/KrillApp/Javascript/view_trips.js
@@ -123,5 +123,83 @@ $(function () {
 
 function export_trip_to_csv(){
 
-    console.log("woops");
+    $.confirm({
+        title: 'Enter valid email address to receive processed CSV file.',
+        content: '' +
+        '<form action="" class="formName">' +
+        '<div class="form-group">' +
+        '<label>Enter something here</label>' +
+        '<input type="text" placeholder="Your email" class="name form-control" required />' +
+        '</div>' +
+        '</form>',
+        buttons: {
+            formSubmit: {
+                text: 'Submit',
+                btnClass: 'btn-blue',
+                action: function () {
+                    var email = this.$content.find('.name').val();
+                    if(!validateEmail(email)){
+                        $.alert('Invalid Email. Please try again!');
+                        return false;
+                    }
+                    $.alert('Your file is being prepared and will be sent to:  ' + email);
+
+                    var trip_to_export = document.getElementById("Trip_Name").innerHTML;
+                    if(trip_to_export !=""){
+
+                        var a = document.createElement('a');
+
+                        $.ajax({
+                            type: "POST",
+                            url: "/export_to_csv/",
+                            data: {
+                                trip: trip_to_export,
+                                'csrfmiddlewaretoken': csrftoken
+                            },
+                            success: function (result) {
+                            }
+                        })
+
+
+    }
+
+
+
+                }
+            },
+            cancel: function () {
+                //close
+            },
+        },
+        onContentReady: function () {
+            // bind to events
+            var jc = this;
+            this.$content.find('form').on('submit', function (e) {
+                // if the user submits the form by pressing enter in the field.
+                e.preventDefault();
+                jc.$$formSubmit.trigger('click'); // reference the button and click it
+            });
+        }
+    });
+
+
+
+  
+
+
+
+
+
+
+
+
+    
+
 }
+
+function validateEmail(email) 
+{
+  var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return re.test(email);
+}
+
